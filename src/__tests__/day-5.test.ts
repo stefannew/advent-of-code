c5969ae5a7eaf84b48e9dfb78cbee0029e4f728e
Stefan New <stfnnew@gmail.com>
2020-12-06 00:36:11 -0500

    Fix day 5 test

--- src/__tests__/day-5.test.ts
+++ src/__tests__/day-5.test.ts
@@ -1,12 +1,10 @@
-import { getSeatNumber } from '../day-5';
+import { getRowColumn } from '../day-5';
 
 describe('Day 5', () => {
-	describe('getSeatNumber', () => {
-		it('should return the seat number for a row and column', () => {
-			expect(getSeatNumber(44, 5)).toEqual(357);
-			expect(getSeatNumber(70, 7)).toEqual(567);
-			expect(getSeatNumber(14, 7)).toEqual(119);
-			expect(getSeatNumber(102, 4)).toEqual(820);
+	describe('getRowColumn', () => {
+		it('should return the row and column', () => {
+			expect(getRowColumn('BFFBBFBRRL')).toEqual([77, 6]);
+			expect(getRowColumn('FFFBFBBLLR')).toEqual([11, 1]);
 		});
 	});
 });
